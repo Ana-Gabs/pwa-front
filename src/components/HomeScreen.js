1dc836500e507a37290feb6988f2a59f1209f251
Ana-Gabs <2022371069@uteq.edu.mx>
2025-09-30 11:02:36 -0600

    Home Sreen

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <h2>Bienvenido a tu To-Do App</h2>
+      <p>Aquí se mostrarán tus tareas.</p>
+    </div>
+  );
+};
+
+export default HomeScreen;
