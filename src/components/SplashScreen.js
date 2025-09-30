18f2274d16ff7226fe92052f60439acd05577b2f
Ana-Gabs <2022371069@uteq.edu.mx>
2025-09-30 11:01:14 -0600

    splash escreen

---
+++ src/components/SplashScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './SplashScreen.css'; 
+
+const SplashScreen = () => {
+  return (
+    <div className="splash-screen">
+      <h1>To-Do IDGS11</h1>
+      <p>Cargando...</p>
+    </div>
+  );
+};
+
+export default SplashScreen;
